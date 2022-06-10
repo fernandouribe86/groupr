0f8cb4ae595d38ef334d2d56035ea493f095c152
Fernando A. Uribe <96088622+fernandouribe86@users.noreply.github.com>
2022-06-10 15:10:19 -0700

    Initial Commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
+import 'react-native-gesture-handler';
+import { useFonts } from 'expo-font';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// IMPORT SCREENS WE WANT TO NAVIGATE
+import Start from './components/Start';
+import Chat from './components/ChatScreen';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+
+render(){
+
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start" >
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat" component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+  },
+  title: {
+    backgroundColor: 'blue',
+  }
+});
