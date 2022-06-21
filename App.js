d127938c5888b936e4a7866ad7d1983b676f2cf1
Fernando A. Uribe <96088622+fernandouribe86@users.noreply.github.com>
2022-06-20 21:38:10 -0700

    Updated App.js

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
 import 'react-native-gesture-handler';
 import { useFonts } from 'expo-font';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import NetInfo from '@react-native-community/netinfo';
 
 // IMPORT SCREENS WE WANT TO NAVIGATE
 import Start from './components/Start';
 import Chat from './components/ChatScreen';
+import { registerRootComponent } from 'expo';
 
 const Stack = createStackNavigator();
 
